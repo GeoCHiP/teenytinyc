797b505385ace62985f6bb6a646649b470a05003
Alexander Stepchenko <x.GeoCHiP.x@yandex.ru>
2024-06-27 01:17:45 +0300

    feat: allow specifying input file to parse

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,45 @@
+#include <fstream>
 #include <iostream>
 
 #include "Lexer.hpp"
+#include "Parser.hpp"
 
-int main() {
-    std::string source = "+- # This is a comment!\n */";
-    Lexer lexer{source};
+std::optional<std::string> ReadFile(const std::string &path) {
+    std::ifstream stream{path};
+    if (!stream) {
+        return std::nullopt;
+    }
 
-    std::optional<Token> result = lexer.GetToken();
-    while (result.has_value() && result.value().Kind != TokenType::Eof) {
-        std::cout << TokenTypeToString(result.value().Kind) << '\n';
-        result = lexer.GetToken();
+    constexpr size_t readSize = 4096;
+    std::string out;
+    std::string buffer(readSize, '\0');
+
+    while (stream.read(&buffer[0], readSize)) {
+        out.append(buffer, 0, stream.gcount());
+    }
+    out.append(buffer, 0, stream.gcount());
+
+    return out;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage:\n    " << argv[0] << " <source_file>\n";
+        return 1;
+    }
+
+    std::optional<std::string> source = ReadFile(argv[1]);
+    if (!source) {
+        std::cerr << "Could not open file: " << argv[1] << '\n';
+        return 1;
+    }
+
+    Lexer lexer{source.value()};
+    Parser parser{lexer};
+
+    try {
+        parser.Program();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
     }
 }
